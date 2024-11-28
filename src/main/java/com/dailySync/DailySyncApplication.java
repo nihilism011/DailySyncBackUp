8ebaf0a35437fce6c222987ed8bdc7cf6d4fcf5f
강선민 <akfnz123@naver.com>
2024-11-28 12:47:26 +0900

    20241128 첫 프로젝트 커밋

---
+++ src/main/java/com/dailySync/DailySyncApplication.java
@@ -0,0 +1,13 @@
+package com.dailySync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailySync/DailySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailySync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
