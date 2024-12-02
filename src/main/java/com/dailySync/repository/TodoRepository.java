b0f8569996b032d16bb4b066ab44682d1adc9ebe
강선민 <akfnz123@naver.com>
2024-12-02 17:25:16 +0900

    delete file

--- src/main/java/com/dailySync/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dailySync.repository;
-
-import com.dailySync.entities.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<TodoItem,Long> {
-}
