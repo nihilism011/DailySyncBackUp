a7c3610fe8117c278f06b14c43a8145bec8d6607
KTH <gaya8962@gmail.com>
2024-12-02 18:01:24 +0900

    식단관리 마이너 수정

--- src/main/java/com/dailySync/meal/repository/MealRepository.java
+++ src/main/java/com/dailySync/meal/repository/MealRepository.java
@@ -2,9 +2,11 @@ package com.dailySync.meal.repository;
 
 import com.dailySync.meal.entities.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
     List<Meal> findByUserId(Long userId);
 }
