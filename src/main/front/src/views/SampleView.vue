0390a25e7404ef9e53be04bb3879459191cc3df8
nihilism011 <nihilism011@gmail.com>
2024-12-06 16:37:21 +0900

    sample 코드 수정

--- src/main/front/src/views/SampleView.vue
+++ src/main/front/src/views/SampleView.vue
@@ -6,17 +6,23 @@
       <input type="text" v-model="inputApiPath" @keyup.enter="fnRequest(inputApiPath)" />
       <button @click="fnRequest(inputApiPath)">요청</button>
     </div>
+    <BoldText value="Hi~everyone" :some="someOne" />
     <div>응답 결과</div>
     <div>{{ result }}</div>
   </div>
 </template>
 <script>
+import BoldText from '../components/sample/BoldText.vue'
 export default {
+  components: {
+    BoldText,
+  },
   data() {
     return {
       result: '',
       inputApiPath: '',
       day: '',
+      someOne: { name: 'kkk', age: 12 },
     }
   },
   methods: {
