f77bdcd20cd9550cac4ea8e6c16e6d1dc63e8371
nihilism011 <nihilism011@gmail.com>
2024-12-07 20:28:23 +0900

    날짜선택기 및 불필요 부분 제거

--- src/main/front/src/router/index.js
+++ src/main/front/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 import SampleView from '@/views/SampleView.vue'
 import AccountView from '@/views/AccountView.vue'
 const router = createRouter({
@@ -10,11 +9,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
     },
     {
       path: '/sample',
--- src/main/front/src/views/AccountView.vue
+++ src/main/front/src/views/AccountView.vue
@@ -1,22 +1,42 @@
 <template>
-  <div class="container">
-    <ItemList account-date="" />
+  <div class="left left-container">
+    <div class="left-top">
+      <DateSelector v-model="selectedDate" />
+    </div>
+    <div class="left-bottom">
+      {{ selectedDate }}
+    </div>
   </div>
+  <div class="right">우측메뉴</div>
 </template>
 <script>
-import ItemList from '@/components/account/ItemList.vue'
+import DateSelector from '@/components/common/DateSelector.vue'
 export default {
   components: {
-    ItemList,
+    DateSelector,
   },
-
   data() {
     return {
-      date: '2024-12-04',
+      selectedDate: this.$dayjs().format('YYYY-MM-DD'),
     }
   },
   methods: {},
   mounted() {},
 }
 </script>
-<style scoped></style>
+<style scoped>
+.left-container {
+  display: flex;
+  flex-direction: column;
+  .left-top {
+    align-content: center;
+    justify-items: center;
+    height: 50%;
+    padding: 20px;
+  }
+  .left-bottom {
+    height: 50%;
+    padding: 20px;
+  }
+}
+</style>
