528b3e478adb0f0024a86ea862775b1b3002a47b
nihilism011 <nihilism011@gmail.com>
2024-12-12 01:01:22 +0900

    달력 수정중

--- src/main/front/src/components/account/leftView/accountCalender.vue
+++ src/main/front/src/components/account/leftView/accountCalender.vue
@@ -6,7 +6,7 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { useDateStore } from '@/stores/dateStore'
-
+import { numToWon } from '@/lib/accountLib'
 export default {
   components: {
     FullCalendar,
@@ -20,8 +20,18 @@ export default {
     const dateStore = useDateStore()
     return { dateStore }
   },
+  watch: {
+    'dateStore.selectedDate': function (newDate) {
+      this.updateCalendarDate(newDate)
+      this.fetchData()
+    },
+    calendarData() {
+      this.calendarOptions.events = this.calendarData
+    },
+  },
   data() {
     return {
+      calendarData: [],
       calendarOptions: {
         plugins: [dayGridPlugin, interactionPlugin],
         initialView: 'dayGridMonth',
@@ -31,31 +41,61 @@ export default {
           center: 'title',
           right: 'next',
         },
-        events: [
-          {
-            title: '임시',
-            start: '2024-12-04',
-          },
-        ],
-        eventClick: this.handleEventClick,
+        events: [],
+        dateClick: this.handleDateClick,
+        dayCellContent: this.dayCellContent,
         datesSet: this.handleDatesChange,
+        footerToolbar: {
+          right: 'today',
+        },
       },
     }
   },
   methods: {
-    handleEventClick: function (info) {
-      if (typeof info === 'string') {
-        this.$emit('fnRequest', info)
-      } else {
-        this.$emit('fnRequest', info.event.startStr)
-      }
+    numToWon,
+    async fetchData() {
+      const year = this.$dayjs(this.dateStore.selectedDate).get('year')
+      const month = this.$dayjs(this.dateStore.selectedDate).get('month') + 1
+      const url = `account/items/month/${year}/${month}`
+      const { data } = await this.$axios.get(url)
+      const transformedData = data.flatMap((item) => {
+        const result = []
+        if (item.plusSumAmount !== 0) {
+          result.push({
+            start: item.accountDate,
+            title: '+' + numToWon(item.plusSumAmount),
+            color: 'red',
+            className: 'asdf',
+          })
+        }
+        if (item.minusSumAmount !== 0) {
+          result.push({
+            start: item.accountDate,
+            title: '-' + numToWon(item.minusSumAmount),
+            color: 'blue',
+          })
+        }
+        return result
+      })
+      this.calendarData = transformedData
+    },
+    handleDateClick(info) {
+      this.dateStore.setSelectedDate(info.dateStr)
     },
     handleDatesChange({ view }) {
       const start = view.currentStart
       const dateSet = `${start.getFullYear()}-${start.getMonth() + 1}-01`
-      this.handleEventClick(dateSet)
+      this.handleDateClick({ dateStr: dateSet })
+    },
+    updateCalendarDate(newDate) {
+      if (this.calendar) {
+        this.calendar.gotoDate(newDate)
+      }
     },
   },
-  mounted() {},
+  mounted() {
+    this.fetchData()
+  },
 }
 </script>
+<style lang="scss" scoped></style>
