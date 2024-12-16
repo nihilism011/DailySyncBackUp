0a1cf235c1c72c2141461fe92ff9620555d1407a
KTH8962 <kth8962@gmail.com>
2024-12-16 20:59:54 +0900

    일정팝업 1차수정

--- src/main/front/src/components/schedule/ScheduleModal.vue
+++ src/main/front/src/components/schedule/ScheduleModal.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="popup-box type-modal" v-if="popupState">
-    <div class="modal-content">
-      <button class="close-button" @click="closeModal">닫기</button>
-      <div v-if="searchResults.length === 0">검색 결과가 없습니다.</div>
-      <div v-else>
-        <div v-for="(item, index) in searchResults" :key="index" class="result-item">
-          <div class="result-title">
-            <div class="title">{{ item.title }}</div>
-            <div class="date-range">
-              {{ formatDate(item.startTime) }} ~ {{ formatDate(item.endTime) }}
+    <div class="popup-cont type2">
+      <div class="popup-tit-wrap">
+        <h2 class="tit">검색결과</h2>
+      </div>
+      <div class="search-list">
+        <div v-if="searchResults.length === 0">검색 결과가 없습니다.</div>
+        <div v-else>
+          <div v-for="(item, index) in searchResults" :key="index" class="result-item">
+            <div class="result-title">
+              <div class="title">{{ item.title }}</div>
+              <div class="date-range">
+                {{ formatDate(item.startTime) }} ~ {{ formatDate(item.endTime) }}
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div class="pop-btn-wrap">
+        <button class="btn-default cancel" @click="closeModal">닫기</button>
+      </div>
     </div>
+    <div class="dimmed">dim</div>
   </div>
 </template>
 
@@ -35,41 +43,4 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-/* 모달 스타일 */
-.popup-box {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 10px;
-  max-width: 600px;
-  width: 100%;
-}
-.close-button {
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-  background-color: white;
-  color: black;
-  border: 1px solid black;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.close-button:hover {
-  background-color: #f0f0f0;
-  border-color: #333;
-}
-</style>
+<style scoped></style>
