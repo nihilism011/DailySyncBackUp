39bb81fe73d2fb61a05d56c81a808c247ccb2f1b
KTH <gaya8962@gmail.com>
2024-12-17 11:36:39 +0900

    todo 추가 css수정

--- src/main/front/src/components/todo/TodoGroup.vue
+++ src/main/front/src/components/todo/TodoGroup.vue
@@ -1,11 +1,13 @@
 <template>
+  <p class="list-tit">Group</p>
   <div class="list-container">
-    <p class="list-tit">Group</p>
-    <div class="list-item" 
-         v-for="(item, index) in list" 
-         :key="index" 
-         :class="{'selected': item.id === selectedGroupId}"  
-         @click="selectGroup(item.id)">
+    <div
+      class="list-item"
+      v-for="(item, index) in list"
+      :key="index"
+      :class="{ selected: item.id === selectedGroupId }"
+      @click="selectGroup(item.id)"
+    >
       <div class="tit-box">
         <div class="description">{{ item.description }}</div>
         <div class="title">{{ item.title }}</div>
@@ -21,12 +23,7 @@
   </div>
 
   <!-- 그룹 추가 모달 -->
-  <Modal
-    :isVisible="isModalVisible"
-    :mode="'create'"
-    @close="closeModal"
-    @save-item="fnMyGroup"
-  />
+  <Modal :isVisible="isModalVisible" :mode="'create'" @close="closeModal" @save-item="fnMyGroup" />
 
   <!-- 그룹 수정 모달 -->
   <Modal
@@ -39,7 +36,7 @@
 </template>
 
 <script>
-import Modal from './GroupCreateModal.vue';
+import Modal from './GroupCreateModal.vue'
 
 export default {
   components: {
@@ -50,61 +47,61 @@ export default {
       list: [],
       isModalVisible: false,
       isEditModalVisible: false,
-      selectedItem: null, 
-      selectedGroupId: null,  // 선택된 그룹의 ID를 추적
-    };
+      selectedItem: null,
+      selectedGroupId: null, // 선택된 그룹의 ID를 추적
+    }
   },
   methods: {
     openAddItemModal() {
-      this.isModalVisible = true;
+      this.isModalVisible = true
     },
     closeModal() {
-      this.isModalVisible = false;
+      this.isModalVisible = false
     },
     openEditModal(item) {
-      this.selectedItem = item; 
-      this.isEditModalVisible = true;
+      this.selectedItem = item
+      this.isEditModalVisible = true
     },
     closeEditModal() {
-      this.isEditModalVisible = false;
-      this.selectedItem = null; 
+      this.isEditModalVisible = false
+      this.selectedItem = null
     },
     async fnMyGroup() {
-      const url = `todo/group`;
-      const { data } = await this.$axios.get(url);
-      this.list = data; 
+      const url = `todo/group`
+      const { data } = await this.$axios.get(url)
+      this.list = data
     },
     selectGroup(groupId) {
-      this.selectedGroupId = groupId;  // 선택된 그룹 ID 저장
-      this.$emit('updateSelectedGroup', groupId);
+      this.selectedGroupId = groupId // 선택된 그룹 ID 저장
+      this.$emit('updateSelectedGroup', groupId)
     },
     async deleteGroup(id) {
-      if (confirm("정말 삭제할거냐? 되돌릴 수 없다!")) {
-        const url = `todo/group/update/status/${id}`;
+      if (confirm('정말 삭제할거냐? 되돌릴 수 없다!')) {
+        const url = `todo/group/update/status/${id}`
         try {
-          await this.$axios.put(url);
-          await this.fnMyGroup(); // 삭제 후 최신 목록을 가져옵니다.
-          this.$emit('updateSelectedGroup', null);
-          alert("그룹 삭제");
+          await this.$axios.put(url)
+          await this.fnMyGroup() // 삭제 후 최신 목록을 가져옵니다.
+          this.$emit('updateSelectedGroup', null)
+          alert('그룹 삭제')
         } catch (error) {
-          alert("삭제 실패: " + error);
+          alert('삭제 실패: ' + error)
         }
       } else {
-        alert("삭제 취소");
+        alert('삭제 취소')
       }
     },
   },
   mounted() {
-    this.fnMyGroup(); 
+    this.fnMyGroup()
   },
-};
+}
 </script>
 
 <style scoped>
 /* 선택된 아이템에 대한 스타일 */
 .selected {
-  background-color: #f0f8ff; 
-  border: 2px solid #007bff; 
+  background-color: #f0f8ff;
+  border: 2px solid #007bff;
 }
 
 .list-item {
--- src/main/front/src/components/todo/TodoItem.vue
+++ src/main/front/src/components/todo/TodoItem.vue
@@ -1,21 +1,21 @@
 <template>
+  <template v-if="!selectedGroup">
+    <p class="list-tit">Select Group</p>
+  </template>
+  <template v-else>
+    <p class="list-tit">Item</p>
+  </template>
   <div class="list-container">
-    <template v-if="!selectedGroup">
-      <p class="list-tit">Select Group</p>
-    </template>
-
     <template v-if="selectedGroup">
-      <p class="list-tit">Item</p>
       <div class="list-item" v-for="(item, index) in items" :key="index">
         <div class="tit-box">
           <div class="title">{{ item.title }}</div>
           <div class="day">
             <span>반복요일: </span>
             <template v-if="item.day && Array.isArray(item.day)">
-              <span 
-                v-for="(day, i) in sortedDays(item.day)" 
-                :key="i" 
-                :class="getDayClass(day)">{{ day }}<span v-if="i < sortedDays(item.day).length - 1">, </span></span>
+              <span v-for="(day, i) in sortedDays(item.day)" :key="i" :class="getDayClass(day)"
+                >{{ day }}<span v-if="i < sortedDays(item.day).length - 1">, </span></span
+              >
             </template>
           </div>
           <div class="auto" title="자동여부">{{ item.isAuto == 1 ? 'A' : '' }}</div>
@@ -26,7 +26,7 @@
         </div>
       </div>
       <div class="list-item center" v-if="selectedGroup" @click="openAddItemModal">
-        <div class="add">아이템 추가 </div>
+        <div class="add">아이템 추가</div>
       </div>
     </template>
 
@@ -34,7 +34,7 @@
     <Modal
       :isVisible="isModalVisible"
       :mode="'create'"
-      :groupId="selectedGroup" 
+      :groupId="selectedGroup"
       @close="closeModal"
       @save-item="fetchItemsByGroup(selectedGroup)"
     />
@@ -44,7 +44,7 @@
       :isVisible="isEditModalVisible"
       :mode="'update'"
       :item="selectedItem"
-      :groupId="selectedGroup" 
+      :groupId="selectedGroup"
       @close="closeEditModal"
       @save-item="fetchItemsByGroup(selectedGroup)"
     />
@@ -52,76 +52,78 @@
 </template>
 
 <script>
-import Modal from './ItemCreateModal.vue';
+import Modal from './ItemCreateModal.vue'
 
 export default {
   components: {
-    Modal
+    Modal,
   },
   props: {
-    selectedGroup: Number
+    selectedGroup: Number,
   },
   data() {
     return {
       items: [],
       isModalVisible: false,
       isEditModalVisible: false,
-      selectedItem: null
-    };
+      selectedItem: null,
+    }
   },
   watch: {
     selectedGroup(newGroupId) {
       if (newGroupId) {
-        this.fetchItemsByGroup(newGroupId);
+        this.fetchItemsByGroup(newGroupId)
       }
     },
   },
   methods: {
     openAddItemModal() {
-      this.isModalVisible = true;
+      this.isModalVisible = true
     },
     closeModal() {
-      this.isModalVisible = false;
+      this.isModalVisible = false
     },
     openEditModal(item) {
-      this.selectedItem = item;
-      this.isEditModalVisible = true;
+      this.selectedItem = item
+      this.isEditModalVisible = true
     },
     closeEditModal() {
-      this.isEditModalVisible = false;
-      this.selectedItem = null;
+      this.isEditModalVisible = false
+      this.selectedItem = null
     },
     async fetchItemsByGroup(groupId) {
-      const url = `todo/item/${groupId}`;
-      const { data } = await this.$axios.get(url);
-      console.log("아이템 목록:", data);
-      this.items = data;
+      const url = `todo/item/${groupId}`
+      const { data } = await this.$axios.get(url)
+      console.log('아이템 목록:', data)
+      this.items = data
     },
     deleteTodoItem(item) {
-      if (confirm("정말 삭제할거냐? 되돌릴 수 없다!")) {
-        const url = `todo/item/update/status/${item.id}`;
-        this.$axios.put(url).then(() => {
-          this.fetchItemsByGroup(item.groupId);
-          alert("아이템 삭제");
-        }).catch((error) => {
-          alert("삭제 실패: " + error);
-        });
+      if (confirm('정말 삭제할거냐? 되돌릴 수 없다!')) {
+        const url = `todo/item/update/status/${item.id}`
+        this.$axios
+          .put(url)
+          .then(() => {
+            this.fetchItemsByGroup(item.groupId)
+            alert('아이템 삭제')
+          })
+          .catch((error) => {
+            alert('삭제 실패: ' + error)
+          })
       } else {
-        alert("삭제 취소");
+        alert('삭제 취소')
       }
     },
     sortedDays(dayArray) {
-      const order = ['일', '월', '화', '수', '목', '금', '토'];
-      return dayArray.sort((a, b) => order.indexOf(a) - order.indexOf(b));
+      const order = ['일', '월', '화', '수', '목', '금', '토']
+      return dayArray.sort((a, b) => order.indexOf(a) - order.indexOf(b))
     },
     getDayClass(day) {
-      if (day === '일') return 'sunday';
-      if (day === '토') return 'saturday';
-      return '';
-    }
+      if (day === '일') return 'sunday'
+      if (day === '토') return 'saturday'
+      return ''
+    },
   },
-};
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
